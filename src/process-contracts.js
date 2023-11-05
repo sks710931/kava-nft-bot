bf2c342f83d92841e8f9111cf9aa28919bd2af3d
Shivam Singh <shivamsingh071093@gmail.com>
2023-11-06 00:47:12 +0530

    log nft's

--- src/process-contracts.js
+++ src/process-contracts.js
@@ -1,12 +1,17 @@
 require("dotenv").config();
 const fs= require("fs");
 const updateCollection = require("./tasks/db/update-erc721-collection");
+const updateNFT = require("./tasks/db/update-nft");
+const model = require("./schema/model");
 
 async function process(){
     let contracts = fs.readFileSync("src/data/contracts.json");
     contracts = JSON.parse(contracts);
     for(let contract of contracts){
-        await updateCollection({address:contract.contractAddress})
+        const nfts = await model.ERC721NFTOwners.findAll({where:{NftContractAddress: contract.contractAddress}});
+        for(let nft of nfts){
+            await updateNFT(nft.dataValues.TokenId, nft.dataValues.NftContractAddress);
+        }
     }
 
 }
